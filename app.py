9814e22c7d880d931ddcaf66d15a978e46dea4e4
Renna Aprilia <rennaaprilia@student.uns.ac.id>
2023-12-12 22:35:56 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/product')
+def index():
+    return render_template('product.html')
+
+@app.route('/admin/customer')
+def index():
+    return render_template('customer.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
